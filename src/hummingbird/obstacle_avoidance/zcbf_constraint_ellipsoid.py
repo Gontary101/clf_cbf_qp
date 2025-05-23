99a2e0f0a784140cc085e78977fbbcbe48c5513b
Gontary101 <gloyejd477@gmail.com>
2025-05-23 08:04:39 +0000

    Enhance ellipsoid obstacle handling in ZCBF filter. Updated parameters in obstacles.yaml for ellipsoid representation and modified zcbf_constraint_ellipsoid.py to utilize these new parameters. Adjusted max_step_size in basic.world for improved simulation performance.

--- src/hummingbird/obstacle_avoidance/zcbf_constraint_ellipsoid.py
+++ src/hummingbird/obstacle_avoidance/zcbf_constraint_ellipsoid.py
@@ -13,22 +13,20 @@ class ZCBFFilter(object):
     def __init__(self, model, obstacles, params, cbf_pub=None):
         self.model = model
         self.obs   = obstacles
-        self.cbf_enter      = rospy.get_param("~cbf_active_range",    2.0)
-        self._hyst_margin   = rospy.get_param("~cbf_hysteresis_margin", 0.5)
+        self.cbf_enter      = rospy.get_param("~cbf_active_range_ellipse",    2.0)
+        self._hyst_margin   = rospy.get_param("~cbf_hysteresis_margin_ellipse", 0.5)
         self.cbf_exit       = self.cbf_enter - self._hyst_margin
         self._obs_active    = [False] * len(self.obs)
-        self.a1    = params.get("zcbf_a1",     1.5)
-        self.a2    = params.get("zcbf_a2",     1.6)
-        self.gamma = params.get("zcbf_gamma",  8.4)
-        self.kappa = params.get("zcbf_kappa",  0.8)
-        self.a     = params.get("zcbf_order_a", 0)
+        self.a1    = params.get("zcbf_a1_ellipse",     1.5)
+        self.a2    = params.get("zcbf_a2_ellipse",     1.6)
+        self.gamma = params.get("zcbf_gamma_ellipse",  8.4)
+        self.kappa = params.get("zcbf_kappa_ellipse",  0.8)
+        self.a     = params.get("zcbf_order_a_ellipse", 0)
         
         self.pub   = cbf_pub
-        self.tau           = params.get("zcbf_tau", 0.2)
         self._last_time    = rospy.get_time()
         self._U_nom_prev   = None
         self._U_out_prev   = None
-        self.s_min         = params.get("zcbf_s_min", -0.5)
         
         self.EPSILON = 1e-9 
         rospy.sleep(0.1)
@@ -44,27 +42,38 @@ class ZCBFFilter(object):
         m, g   = self.model.m, self.model.g
         J      = self.model.J_mat
         Jinv   = self.model.J_inv_diag
-
+        kf, km = self.model.kf, self.model.km
+        w_max  = self.model.w_max
+        
         thr_min_fac = rospy.get_param("~min_thrust_factor", 0.10)
-        U1_min      = thr_min_fac * m * g
+        U1_min_val  = float(thr_min_fac) * m * g
 
-        p = state["p_vec"];  v = state["v_vec"]
-        R = state["R_mat"];  Om = state["omega_body"]
+        p_vec = state["p_vec"];  v_vec = state["v_vec"]
+        R_mat = state["R_mat"];  Om_body = state["omega_body"]
+        
+        RT_mat = R_mat.T
 
         G_list, h_list = [], []
 
-        q = R[:, 2]
-        R_Omxe3 = R.dot(np.cross(Om, e3_body))
-
-        Om_cross_e3           = np.cross(Om, e3_body)
-        Om_cross_Om_cross_e3  = np.cross(Om, Om_cross_e3)
-        R_Om_cross2           = np.dot(R, Om_cross_Om_cross_e3)
+        q_vec = R_mat[:, 2]
         
-        OmJ                   = np.dot(J, Om)
-        Om_cross_JOm          = np.cross(Om, OmJ)
-        xi                    = np.dot(Jinv, Om_cross_JOm)
-        R_xi_cross_e3         = np.dot(R, np.cross(xi, e3_body))
+        Om_cross_e3_val          = np.cross(Om_body, e3_body)
+        R_Omxe3_val              = R_mat.dot(Om_cross_e3_val)
+        
+        Om_cross_Om_cross_e3_val = np.cross(Om_body, Om_cross_e3_val)
+        R_Om_cross2_val          = R_mat.dot(Om_cross_Om_cross_e3_val)
+        
+        J_Om_val                 = J.dot(Om_body)
+        Om_cross_JOm_val         = np.cross(Om_body, J_Om_val)
+        xi_val                   = Jinv.dot(Om_cross_JOm_val) 
+        
+        xi_cross_e3_val          = np.cross(xi_val, e3_body)
+        R_xi_cross_e3_val        = R_mat.dot(xi_cross_e3_val)
 
+        cbf_enter_sq = self.cbf_enter**2
+        cbf_exit_sq = self.cbf_exit**2
+        
+        a1_a2_val = self.a1 * self.a2
 
         for i, o in enumerate(self.obs):
             ox, oy, oz = o[0], o[1], o[2]
@@ -72,112 +81,141 @@ class ZCBFFilter(object):
             ax, ay, az = o[6], o[7], o[8]
             obs_a, obs_b, obs_c, obs_n = o[9], o[10], o[11], o[12]
 
-            x_o = np.array([ox, oy, oz])
-            V_o = np.array([vx, vy, vz])
-            A_o = np.array([ax, ay, az])
+            x_o_vec = np.array([ox, oy, oz])
+            V_o_vec = np.array([vx, vy, vz])
+            A_o_vec = np.array([ax, ay, az])
 
-            dist_sq = np.sum((p - x_o)**2)
+            r_vec = p_vec - x_o_vec
+            
+            dist_sq_val = np.sum(r_vec**2)
             if not self._obs_active[i]:
-                if dist_sq > self.cbf_enter**2:
+                if dist_sq_val > cbf_enter_sq:
                     continue
                 else:
                     self._obs_active[i] = True
             else:
-                if dist_sq > self.cbf_exit**2:
+                if dist_sq_val > cbf_exit_sq:
                     self._obs_active[i] = False
                     continue
             
-            r = p - x_o
-            r_dot = v - V_o
+            r_dot_vec = v_vec - V_o_vec
 
-            s = np.dot(r, q)
-            sigma = -self.a1 * math.atan(self.a2 * s)
+            s_val = np.dot(r_vec, q_vec)
+            a2_s_val = self.a2 * s_val
             
-            sig_p_denominator = (1.0 + (self.a2 * s)**2)
-            sig_p = -self.a1 * self.a2 / sig_p_denominator
-            sig_pp = 2.0 * self.a1 * (self.a2**3) * s / (sig_p_denominator**2)
-
-            val_ax_abs = abs(r[0]/obs_a)
-            val_by_abs = abs(r[1]/obs_b)
-            val_cz_abs = abs(r[2]/obs_c)
-
-            Phi = np.power(val_ax_abs, obs_n) + \
-                  np.power(val_by_abs, obs_n) + \
-                  np.power(val_cz_abs, obs_n)
-
-            g_hat = Phi - 1.0 - sigma
-
-            grad_Phi = np.array([
-                (obs_n/obs_a) * np.sign(r[0]) * np.power(val_ax_abs, obs_n-1),
-                (obs_n/obs_b) * np.sign(r[1]) * np.power(val_by_abs, obs_n-1),
-                (obs_n/obs_c) * np.sign(r[2]) * np.power(val_cz_abs, obs_n-1)
-            ])
-
-            dot_s_calc = np.dot(r_dot, q) + np.dot(r, R_Omxe3)
+            sigma_calc_val = -self.a1 * math.atan(a2_s_val)
             
-            g_hat_d = np.dot(grad_Phi, r_dot) - sig_p * dot_s_calc
-            
-            h_val = self.gamma * g_hat + g_hat_d
-            
-            Gamma1 = (np.dot(grad_Phi, q) - sig_p) / m
-            
-            r_b = np.dot(R.T, r)
-            Gamma2_vec = sig_p * np.dot(np.cross(r_b, e3_body), Jinv)
+            sig_p_denominator_val = (1.0 + a2_s_val**2)
+            sig_p_val = -a1_a2_val / sig_p_denominator_val
+            sig_pp_val = -2.0 * sig_p_val * self.a2 * a2_s_val / sig_p_denominator_val
 
-            term1 = self.gamma * g_hat_d
+            val_ax_abs = abs(r_vec[0]/obs_a)
+            val_by_abs = abs(r_vec[1]/obs_b)
+            val_cz_abs = abs(r_vec[2]/obs_c)
 
-            n_factor = obs_n * (obs_n - 1)
-            H_xx = (n_factor / (obs_a**2)) * np.power(val_ax_abs + self.EPSILON if obs_n < 2 else val_ax_abs, obs_n - 2)
-            H_yy = (n_factor / (obs_b**2)) * np.power(val_by_abs + self.EPSILON if obs_n < 2 else val_by_abs, obs_n - 2)
-            H_zz = (n_factor / (obs_c**2)) * np.power(val_cz_abs + self.EPSILON if obs_n < 2 else val_cz_abs, obs_n - 2)
-            term2_new = H_xx * r_dot[0]**2 + H_yy * r_dot[1]**2 + H_zz * r_dot[2]**2
+            pow_val_ax_abs_obs_n = np.power(val_ax_abs, obs_n)
+            pow_val_by_abs_obs_n = np.power(val_by_abs, obs_n)
+            pow_val_cz_abs_obs_n = np.power(val_cz_abs, obs_n)
+            Phi_calc_val = pow_val_ax_abs_obs_n + pow_val_by_abs_obs_n + pow_val_cz_abs_obs_n
+
+            g_hat_calc_val = Phi_calc_val - 1.0 - sigma_calc_val
+
+            common_obs_n_minus_1 = obs_n - 1
+            pow_val_ax_abs_obs_n_m1 = np.power(val_ax_abs, common_obs_n_minus_1)
+            pow_val_by_abs_obs_n_m1 = np.power(val_by_abs, common_obs_n_minus_1)
+            pow_val_cz_abs_obs_n_m1 = np.power(val_cz_abs, common_obs_n_minus_1)
+
+            grad_Phi_x_val = (obs_n/obs_a) * np.sign(r_vec[0]) * pow_val_ax_abs_obs_n_m1
+            grad_Phi_y_val = (obs_n/obs_b) * np.sign(r_vec[1]) * pow_val_by_abs_obs_n_m1
+            grad_Phi_z_val = (obs_n/obs_c) * np.sign(r_vec[2]) * pow_val_cz_abs_obs_n_m1
+            grad_Phi_vec_val = np.array([grad_Phi_x_val, grad_Phi_y_val, grad_Phi_z_val])
+
+            dot_r_dot_q_val = np.dot(r_dot_vec, q_vec)
+            dot_r_R_Omxe3_val = np.dot(r_vec, R_Omxe3_val)
+            dot_s_calc_val = dot_r_dot_q_val + dot_r_R_Omxe3_val
             
-            term3_new = -g * grad_Phi[2]
-
-            term4_new = -np.dot(grad_Phi, A_o)
-
-            term5 = -sig_pp * (dot_s_calc**2)
-
-            term6 = sig_p * g * q[2]
-
-            term_Ao_q_sig_p_new = sig_p * np.dot(A_o, q)
-
-            term7 = -sig_p * 2.0 * np.dot(r_dot, R_Omxe3)
+            dot_grad_Phi_r_dot_val = np.dot(grad_Phi_vec_val, r_dot_vec)
+            g_hat_d_calc_val = dot_grad_Phi_r_dot_val - sig_p_val * dot_s_calc_val
             
-            term8 = -sig_p * np.dot(r, R_Om_cross2)
+            h_internal_val = self.gamma * g_hat_calc_val + g_hat_d_calc_val
             
-            term9 = sig_p * np.dot(r, R_xi_cross_e3)
+            dot_grad_Phi_q_val = np.dot(grad_Phi_vec_val, q_vec)
+            Gamma1_calc_val = (dot_grad_Phi_q_val - sig_p_val) / m
+            
+            r_b_vec_val = RT_mat.dot(r_vec)
+            
+            cross_r_b_e3_val = np.cross(r_b_vec_val, e3_body)
+            Gamma2_vec_calc_val = sig_p_val * np.dot(cross_r_b_e3_val, Jinv)
 
-            L_f_sq_g_hat = (term2_new + term3_new + term4_new + term5 + term6 + 
-                            term_Ao_q_sig_p_new + term7 + term8 + term9)
+            term1_calc_val = self.gamma * g_hat_d_calc_val
+
+            n_factor_val = obs_n * (obs_n - 1)
+            common_obs_n_minus_2 = obs_n - 2
+
+            base_H_xx_val = val_ax_abs + self.EPSILON if obs_n < 2 else val_ax_abs
+            base_H_yy_val = val_by_abs + self.EPSILON if obs_n < 2 else val_by_abs
+            base_H_zz_val = val_cz_abs + self.EPSILON if obs_n < 2 else val_cz_abs
             
-            Gamma3_code = term1 + L_f_sq_g_hat
+            H_xx_calc_val = (n_factor_val / (obs_a**2)) * np.power(base_H_xx_val, common_obs_n_minus_2)
+            H_yy_calc_val = (n_factor_val / (obs_b**2)) * np.power(base_H_yy_val, common_obs_n_minus_2)
+            H_zz_calc_val = (n_factor_val / (obs_c**2)) * np.power(base_H_zz_val, common_obs_n_minus_2)
             
-            current_G_row = np.hstack([Gamma1, Gamma2_vec])
+            r_dot0_sq_val = r_dot_vec[0]**2
+            r_dot1_sq_val = r_dot_vec[1]**2
+            r_dot2_sq_val = r_dot_vec[2]**2
+            term2_new_calc_val = H_xx_calc_val * r_dot0_sq_val + H_yy_calc_val * r_dot1_sq_val + H_zz_calc_val * r_dot2_sq_val
+            
+            term3_new_calc_val = -g * grad_Phi_vec_val[2]
+
+            dot_grad_Phi_A_o_val = np.dot(grad_Phi_vec_val, A_o_vec)
+            term4_new_calc_val = -dot_grad_Phi_A_o_val
+
+            dot_s_calc_sq_val = dot_s_calc_val**2
+            term5_calc_val = -sig_pp_val * dot_s_calc_sq_val
+
+            term6_calc_val = sig_p_val * g * q_vec[2]
+
+            dot_A_o_q_val = np.dot(A_o_vec, q_vec)
+            term_Ao_q_sig_p_new_calc_val = sig_p_val * dot_A_o_q_val
+
+            dot_r_dot_R_Omxe3_val = np.dot(r_dot_vec, R_Omxe3_val)
+            term7_calc_val = -sig_p_val * 2.0 * dot_r_dot_R_Omxe3_val
+            
+            dot_r_R_Om_cross2_val = np.dot(r_vec, R_Om_cross2_val)
+            term8_calc_val = -sig_p_val * dot_r_R_Om_cross2_val
+            
+            dot_r_R_xi_cross_e3_val = np.dot(r_vec, R_xi_cross_e3_val)
+            term9_calc_val = sig_p_val * dot_r_R_xi_cross_e3_val
+
+            L_f_sq_g_hat_calc_val = (term2_new_calc_val + term3_new_calc_val + term4_new_calc_val + term5_calc_val + term6_calc_val + 
+                                     term_Ao_q_sig_p_new_calc_val + term7_calc_val + term8_calc_val + term9_calc_val)
+            
+            Gamma3_code_calc_val = term1_calc_val + L_f_sq_g_hat_calc_val
+            
+            current_G_row_val = np.hstack([Gamma1_calc_val, Gamma2_vec_calc_val])
             
             h_val_pow_exponent = 2 * self.a + 1
             if h_val_pow_exponent == 1:
-                 h_val_pow = h_val
+                 h_val_pow_calc_val = h_internal_val
             else:
-                 h_val_pow = np.sign(h_val) * np.power(abs(h_val), h_val_pow_exponent)
+                 h_val_pow_calc_val = np.sign(h_internal_val) * np.power(abs(h_internal_val), h_val_pow_exponent)
 
+            current_h_final_val = Gamma3_code_calc_val + self.kappa * h_val_pow_calc_val
 
-            current_h_value = Gamma3_code + self.kappa * h_val_pow
+            G_list.append(-current_G_row_val)
+            h_list.append(current_h_final_val)
 
-            G_list.append(-current_G_row)
-            h_list.append(current_h_value)
+        U1_max_val = 4.0 * kf * w_max ** 2
 
-        U1_max = 4.0 * self.model.kf * self.model.w_max**2
+        G_box_val  = np.array([[ 1., 0, 0, 0],
+                               [-1., 0, 0, 0]])
+        h_box_val  = np.array([[ float(U1_max_val)],
+                               [-float(U1_min_val)]])
 
-        G_box  = np.array([[ 1., 0, 0, 0],
-                           [-1., 0, 0, 0]])
-        h_box  = np.array([[ U1_max],
-                           [-U1_min]])
+        G_cbf_val = np.vstack(G_list) if G_list else np.empty((0, 4))
+        h_cbf_val = np.array(h_list, dtype=float).reshape(-1, 1) if h_list else np.empty((0,1))
 
-        G_cbf = np.vstack(G_list) if G_list else np.empty((0, 4))
-        h_cbf = np.array(h_list, dtype=float).reshape(-1, 1) if h_list else np.empty((0,1))
-
-        G_all = np.vstack([G_cbf, G_box])
-        h_all = np.vstack([h_cbf, h_box])
+        G_all_val = np.vstack([G_cbf_val, G_box_val])
+        h_all_val = np.vstack([h_cbf_val, h_box_val])
         
-        return G_all, h_all
+        return G_all_val, h_all_val
